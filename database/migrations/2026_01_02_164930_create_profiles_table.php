176b9e30726954ab1992a916d27e308f28cc30a5
ainazrn <nurrrainaaa55@gmail.com>
2026-01-03 02:04:46 +0800

    user_id in profile

--- database/migrations/2026_01_02_164930_create_profiles_table.php
+++ database/migrations/2026_01_02_164930_create_profiles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('profiles', function (Blueprint $table) {
-            $table->id();
+            $table->id('user_id');
             $table->string('fullname');
             $table->string('phone');
             $table->string('email');
